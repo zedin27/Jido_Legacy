955a38a0dace22cece8d3c869af3143f6b943f92
zedin27 <zeidtisnes@gmail.com>
2023-08-22 14:42:42 -0700

    add: URL parser of the title. Next is to click the link and extract the content of the title

--- webscrapping.py
+++ webscrapping.py
@@ -1,7 +1,16 @@
 '''
-Little project to help and maintain my grandfather's legacy in journalism
+Little project to help and maintain my grandfather's legacy in journalism.
+I will leave few resource links that I had to go through in my testing:
+	Old project like Ford_hackathon for pip and virtual environment
+	Python documentation for urllib library
 '''
+
 from urllib.request import urlopen
+import urllib.parse
+from bs4 import BeautifulSoup
+import lxml
+
+#NOTE: How can I make a safe global variable for my URL to avoid any injection?
 
 def page_number(pg_number=None):
 	'''Return the current page number'''
@@ -16,10 +25,53 @@ def html_content():
 
 def title(arr=None, idx=None):
 	'''Return index (idx) from the list of titles (arr)'''
-	return
+	url = "https://alrai.com/author/19/%D8%AF-%D8%B2%D9%8A%D8%AF-%D8%AD%D9%85%D8%B2%D8%A9"
+	content = html_content()
+	soup = BeautifulSoup(content, 'html.parser')
+	links = soup.find_all('a', class_='size-17 font-700')
+	print(links)
+
+	for link in links:
+		link_url = link.get('href')
+		if link_url:
+			if not link_url.startswith('http'):
+				link_url = urllib.parse.urljoin(url, link_url)
+				
+	return link_url
 
 def paragraph(idx=None): #NOTE: maybe have this in the same title function?
 	return
+
+def find(content, target, target2, beginning, end):
+	'''
+	HTML content with two target values (one that starts with and other that ends), beginning and ending index for parsing
+	Need to work more on this portion of the code if it becomes unreadable
+	'''
+
+	content = html_content()
+	target = '<div class="title-article">'
+	target2 = '</div>'
+	beginning = 0
+	titles_link = []
+
+	while beginning != -1:
+		beginning = content.find(target, beginning)
+
+		if beginning != -1:
+			beginning += len(target)
+			end = content.find(target2, beginning)
+
+			if end != -1:
+				content_title_article = content[beginning:end].strip()
+				title_start = content_title_article.find('title="') + len('title="')
+				title_end = content_title_article.find('>"', title_start)
+				title_name = content_title_article[title_start:title_end].replace('\r', '')
+				titles_link.append(title_name)
+			else:
+				print("End tag not found")
+		else:
+			print("Title element was not found")
+	return titles_link
 
 def jiddo_legacy():
 	'''
@@ -29,11 +81,11 @@ def jiddo_legacy():
 
 	# url = "https://alrai.com/author/19/د-زيد-حمزة" #NOTE: I need to test how to parse non-ASCII characters
 	content = html_content()
-	titles = []
+	titles_links = []
 	target_element_title_article = '<div class="title-article">'
 	start_index = 0
 	page_number = 0 # After reading the articles, go to the next page number (should be after 10 or less)
-
+	
 	while start_index != -1:
 		start_index = content.find(target_element_title_article, start_index)
 		
@@ -45,16 +97,22 @@ def jiddo_legacy():
 				content_title_article = content[start_index:end_index].strip()
 				title_start = content_title_article.find('title="') + len('title="')
 				title_end = content_title_article.find('">', title_start)
-				title = content_title_article[title_start:title_end]
-				title = title.replace('\r', '').strip()
-				titles.append(title)
+				title = content_title_article[title_start:title_end].replace('\r', '')
+
+				link_start = content_title_article.find('href="') + len('href="')
+				link_end = content_title_article.find('"', link_start)
+				link = content_title_article[link_start:link_end].replace('\r', '')
+				if not link.startswith('https'):
+					link = urllib.parse.urljoin("https://alrai.com", link)
+				titles_links.append((title, link))
+				# titles_links.append(title)
 			else:
 				print("End tag not found")
 		else:
 			print("Title element was not found")
 
-	for i, title_name in enumerate(titles):
-		print(f"Title {i+1}: {title_name}")
-	print(titles)
+	for i, (title_name, link) in enumerate(titles_links):
+		print(f"Title w/ URL {i+1}: {title_name} ({link})")
 
 jiddo_legacy()
+# title()
